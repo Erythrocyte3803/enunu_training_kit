0a47a31ec1f7f3f9d6fe8e0246ac814f65efda91
Auxthority <100791943+Auxthority@users.noreply.github.com>
2022-07-12 17:42:27 -0400

    Update install.sh

--- train/install.sh
+++ train/install.sh
@@ -1,6 +1,11 @@
-apt update
+apt update &> /dev/null
+echo "installing 7z"
 DEBIAN_FRONTEND=noninteractive apt-get install -qq p7zip-full p7zip-rar &> /dev/null
+echo "updating pip"
 pip install --upgrade pip &> /dev/null
-
+echo "Installing Bandmat"
+%pip install https://github.com/MattShannon/bandmat/archive/master.zip &> /dev/null
+echo "installing nnsvs main branch"
+pip install -I https://github.com/nnsvs/nnsvs/archive/master.zip &> /dev/null
+echo "installing literally everything else (this will take a bit...)"
 pip install wheel numpy cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow utaupy tqdm pydub pyyaml natsort github-clone torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113 &> /dev/null
-pip install -I git+https://github.com/r9y9/nnsvs.git &> /dev/null
