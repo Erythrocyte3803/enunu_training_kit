2b1d2b4ff153569ebfc2d172ec4fb87c56fc31ad
Auxthority <100791943+Auxthority@users.noreply.github.com>
2022-07-12 18:55:27 -0400

    Update run.sh

--- train/run.sh
+++ train/run.sh
@@ -77,6 +77,7 @@ fi
 
 # Skipping synthesis step.
 
+# Postfilter stuff
 if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
     echo "stage 6: Prepare input/output features for post-filter"
     . $NNSVS_COMMON_ROOT/prepare_postfilter.sh
@@ -87,7 +88,67 @@ if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
     . $NNSVS_COMMON_ROOT/train_postfilter.sh
 fi
 
+# Vocoder stuff
+if [ ${stage} -le 8 ] && [ ${stop_stage} -ge 8 ]; then
+    echo "stage 8: Prepare vocoder input/output features"
+    . $NNSVS_COMMON_ROOT/prepare_voc_features.sh
+fi
+
+if [ ${stage} -le 9 ] && [ ${stop_stage} -ge 9 ]; then
+    echo "stage 9: Compute statistics of vocoder's input features"
+
+    if [[ ${acoustic_features} == *"static_deltadelta_sinevib"* ]]; then
+        ext="--num_windows 3 --vibrato_mode sine"
+    elif [[ ${acoustic_features} == *"static_deltadelta_diffvib"* ]]; then
+        ext="--num_windows 3 --vibrato_mode diff"
+    elif [[ ${acoustic_features} == *"static_only_sinevib"* ]]; then
+        ext="--num_windows 1 --vibrato_mode sine"
+    elif [[ ${acoustic_features} == *"static_only_diffvib"* ]]; then
+        ext="--num_windows 1 --vibrato_mode diff"
+    elif [[ ${acoustic_features} == *"static_deltadelta"* ]]; then
+        ext="--num_windows 3 --vibrato_mode none"
+    elif [[ ${acoustic_features} == *"static_only"* ]]; then
+        ext="--num_windows 1 --vibrato_mode none"
+    else
+        ext=""
+    fi
+
+    xrun python $NNSVS_COMMON_ROOT/scaler_joblib2npy_voc.py \
+        $dump_norm_dir/out_acoustic_scaler.joblib $dump_norm_dir/ \
+        --sample_rate $sample_rate $ext
+fi
+
 if [ ${stage} -le 10 ] && [ ${stop_stage} -ge 10 ]; then
-    echo "#  stage 7: Release preparation          #"
+    echo "stage 10: Training vocoder using parallel_wavegan"
+    if [ ! -z ${pretrained_vocoder_checkpoint} ]; then
+        extra_args="--resume $pretrained_vocoder_checkpoint"
+    else
+        extra_args=""
+    fi
+    # NOTE: copy normalization stats to expdir for convenience
+    mkdir -p $expdir/$vocoder_model
+    cp -v $dump_norm_dir/in_vocoder*.npy $expdir/$vocoder_model
+    xrun parallel-wavegan-train --config conf/parallel_wavegan/${vocoder_model}.yaml \
+        --train-dumpdir $dump_norm_dir/$train_set/in_vocoder \
+        --dev-dumpdir $dump_norm_dir/$dev_set/in_vocoder/ \
+        --outdir $expdir/$vocoder_model $extra_args
+fi
+
+#if [ ${stage} -le 11 ] && [ ${stop_stage} -ge 11 ]; then
+#    echo "stage 11: Synthesis waveforms by parallel_wavegan"
+#    if [ -z "${vocoder_eval_checkpoint}" ]; then
+#        vocoder_eval_checkpoint="$(ls -dt "${expdir}/${vocoder_model}"/*.pkl | head -1 || true)"
+#    fi
+#    outdir="${expdir}/$vocoder_model/wav/$(basename "${vocoder_eval_checkpoint}" .pkl)"
+#    for s in ${testsets[@]}; do
+#        xrun parallel-wavegan-decode --dumpdir $dump_norm_dir/$s/in_vocoder \
+#            --checkpoint $vocoder_eval_checkpoint \
+#            --outdir $outdir
+#    done
+#fi
+
+#Pack it up
+if [ ${stage} -le 12 ] && [ ${stop_stage} -ge 12 ]; then
+    echo "#  stage 12: Release preparation          #"
     python prepare_release.py $CONFIG_PATH || exit 1;
 fi
