c14384c21c7105340df2f31fe92c35ae125511d1
Auxthority <100791943+Auxthority@users.noreply.github.com>
2022-07-22 21:25:49 -0400

    Update train_postfilter.sh

--- train/scripts/train_postfilter.sh
+++ train/scripts/train_postfilter.sh
@@ -31,14 +31,13 @@ fi
 
 xrun nnsvs-train-postfilter $ext $hydra_opt \
     model=$postfilter_model train=$postfilter_train data=$postfilter_data \
-    data.train_no_dev.in_dir=$expdir/$acoustic_model/norm/$train_set/in_postfilter \
+    data.train_no_dev.in_dir=$expdir/acoustic/norm/$train_set/in_postfilter \
     data.train_no_dev.out_dir=$dump_norm_dir/$train_set/out_postfilter \
-    data.dev.in_dir=$expdir/$acoustic_model/norm/$dev_set/in_postfilter \
+    data.dev.in_dir=$expdir/acoustic/norm/$dev_set/in_postfilter \
     data.dev.out_dir=$dump_norm_dir/$dev_set/out_postfilter \
-    data.in_scaler_path=$expdir/$acoustic_model/norm/in_postfilter_scaler.joblib \
+    data.in_scaler_path=$expdir/acoustic/norm/in_postfilter_scaler.joblib \
     data.out_scaler_path=$dump_norm_dir/out_postfilter_scaler.joblib \
     data.sample_rate=$sample_rate \
     train.out_dir=$expdir/${postfilter_model} \
-    train.log_dir=tensorboard/${expname}_${postfilter_model} \
     train.resume.netG.checkpoint=$resume_checkpoint_g \
     train.resume.netD.checkpoint=$resume_checkpoint_d $post_args
